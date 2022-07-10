a917da4cab20241b5a2cbc0228dad7527cf97b93
Gurkan Kaya <kayagurkan05@gmail.com>
2022-07-11 00:06:57 +0300

    web3-todo-app

--- client/src/Task.js
+++ client/src/Task.js
@@ -1,5 +1,23 @@
+import { List, ListItem , ListItemAvatar,  ListItemText } from '@mui/material';
 import './Task.css';
-const Task =({}) => {
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const Task =({taskText,onClick}) => {
+
+    return(
+        <List className="todo_list">
+            
+            <ListItem>
+            
+            <ListItemText primary ={taskText}/>
+
+            </ListItem>
+
+            <DeleteIcon fontSize ="large" style={{opacity:0.7}} onClick={onClick}/>
+
+
+        </List>
+    )
 
 }
 
